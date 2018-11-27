796c0dfec6d9fd3fa75232475016621885365cf9
Cause-why-not <45392868+Cause-why-not@users.noreply.github.com>
2018-11-27 19:34:56 +0530

    Add files via upload

---
+++ IR_sensor led.ino
@@ -0,0 +1,28 @@
+int i;
+void setup() {
+pinMode(5, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+Serial.println(" hi");  
+i=digitalRead(5);
+if(i==0)
+{
+  light1();
+}
+else if(i==1)
+{
+ light(); 
+}
+}
+
+void light()
+{
+  analogWrite(6, 100);
+}
+void light1()
+{
+  analogWrite(6, 10);
+}
+
---
+++ hcsr04_1.ino
@@ -0,0 +1,41 @@
+long t, d;
+
+const int tr = 9;
+const int e = 10;
+void setup() {
+pinMode(9,OUTPUT);
+pinMode(10,INPUT);
+Serial.begin(9600);  
+
+}
+
+void loop() {
+
+digitalWrite(tr,LOW);
+delayMicroseconds(2);
+
+digitalWrite(tr, HIGH);
+delayMicroseconds(10);
+digitalWrite(tr, LOW);
+t = pulseIn(e, HIGH);
+d= t*0.034/2;
+Serial.println(d);
+if(d>10)
+{
+  light();
+}
+else
+{
+  light1();
+}
+
+}
+
+void light()
+{
+  analogWrite(6, 100);
+}
+void light1()
+{
+  analogWrite(6, 10);
+}
